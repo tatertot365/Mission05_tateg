596fe8331b9cade5812510c424ea651db4525df0
tatertot365 <tate.gillespie@gmail.com>
2023-01-25 18:42:17 -0700

    first commit

---
+++ Mission04/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mission04.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+
